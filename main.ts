85cc71eb04a7f0030927d264265ed579f4f075be
TutiPY <matuteramalho@gmail.com>
2025-05-16 16:16:21 -0300

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,30 @@
+input.onGesture(Gesture.TiltRight, function () {
+    basic.showArrow(ArrowNames.East)
+})
+input.onGesture(Gesture.TiltLeft, function () {
+    basic.showArrow(ArrowNames.West)
+})
+basic.forever(function () {
+    if (input.isGesture(Gesture.TiltLeft)) {
+        basic.showIcon(IconNames.Yes)
+        basic.pause(100)
+        music.play(music.stringPlayable("E D E C G E F D ", 300), music.PlaybackMode.UntilDone)
+        basic.pause(100)
+    } else {
+        basic.showIcon(IconNames.No)
+        basic.pause(100)
+        music.play(music.stringPlayable("- B C5 B G B G C5 ", 300), music.PlaybackMode.UntilDone)
+        basic.pause(100)
+    }
+    if (input.isGesture(Gesture.TiltRight)) {
+        basic.showIcon(IconNames.Yes)
+        basic.pause(100)
+        music.play(music.stringPlayable("E D E C G E F D ", 300), music.PlaybackMode.UntilDone)
+        basic.pause(100)
+    } else {
+        basic.showIcon(IconNames.No)
+        basic.pause(100)
+        music.play(music.stringPlayable("- B C5 B G B G C5 ", 300), music.PlaybackMode.UntilDone)
+        basic.pause(100)
+    }
+})
